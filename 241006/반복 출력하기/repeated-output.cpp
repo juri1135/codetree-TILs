08cc87ade6c1baa542c6cd416e5388eaff53ebc0
juri1135 <81513165+juri1135@users.noreply.github.com>
2024-10-06 16:43:32 +0900

    Title: 반복 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241006/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+void func(int n){
+    for(int i=0; i<n; i++){
+        printf("12345^&*()_\n");
+    }
+}
+int main() {
+    int n;
+   cin>>n;
+   func(n);
+}
