e8ec9e85b11bd9aa5e033f7c735ee8c5ef979b64
juri1135 <81513165+juri1135@users.noreply.github.com>
2024-11-14 12:28:34 +0900

    Title: 재귀함수를 이용한 피보나치 수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241114/재귀함수를 이용한 피보나치 수/fibonacci-using-recursive-function.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int fibonacci(int n) {
+    if (n == 1 || n == 2) {
+        return 1;
+    }
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int main() {
+    int N;
+    cin >> N;
+    cout << fibonacci(N) << endl;
+    return 0;
+}
