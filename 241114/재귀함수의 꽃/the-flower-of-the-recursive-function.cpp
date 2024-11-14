462535e836276dacb35cd1c367de719d76ae8820
juri1135 <81513165+juri1135@users.noreply.github.com>
2024-11-14 12:28:22 +0900

    Title: 재귀함수의 꽃, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241114/재귀함수의 꽃/the-flower-of-the-recursive-function.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+void print_sequence(int n) {
+    if (n == 0) {
+        return;
+    }
+    cout << n << " ";
+    print_sequence(n - 1);
+    if (n != 1) {
+        cout << n << " ";
+    }
+}
+
+int main() {
+    int N;
+    cin >> N;
+    print_sequence(N);
+    cout << endl;
+    return 0;
+}
