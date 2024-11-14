6f7023317d94a785b6a35b464d077d58d5ef381e
juri1135 <81513165+juri1135@users.noreply.github.com>
2024-11-14 12:31:48 +0900

    Title: 1이 되는 순간까지, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 241114/1이 되는 순간까지/until-the-moment-I-reach-one.cpp
+++ 241114/1이 되는 순간까지/until-the-moment-I-reach-one.cpp
@@ -2,33 +2,6 @@
 
 using namespace std;
 
-void print_ascending(int n) {
-    if (n == 0) {
-        return;
-    }
-    print_ascending(n - 1);
-    cout << n << " ";
-}
-
-void print_descending(int n) {
-    if (n == 0) {
-        return;
-    }
-    cout << n << " ";
-    print_descending(n - 1);
-}
-
-void print_stars(int n, int current = 1) {
-    if (current > n) {
-        return;
-    }
-    for (int i = 0; i < current; ++i) {
-        cout << "*";
-    }
-    cout << endl;
-    print_stars(n, current + 1);
-}
-
 int count_steps(int n) {
     if (n == 1) {
         return 0;
@@ -43,11 +16,6 @@ int count_steps(int n) {
 int main() {
     int N;
     cin >> N;
-    print_ascending(N);
-    cout << endl;
-    print_descending(N);
-    cout << endl;
-    print_stars(N);
     cout << count_steps(N) << endl;
     return 0;
 }
