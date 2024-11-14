402f87c2766d1110482d20d32fa64516e98aa6b9
juri1135 <81513165+juri1135@users.noreply.github.com>
2024-11-14 12:27:31 +0900

    Title: 재귀함수를 이용한 별 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241114/재귀함수를 이용한 별 출력/star-output-with-recursive-function.cpp
+++ 241114/재귀함수를 이용한 별 출력/star-output-with-recursive-function.cpp
@@ -2,22 +2,6 @@
 
 using namespace std;
 
-void print_ascending(int n) {
-    if (n == 0) {
-        return;
-    }
-    print_ascending(n - 1);
-    cout << n << " ";
-}
-
-void print_descending(int n) {
-    if (n == 0) {
-        return;
-    }
-    cout << n << " ";
-    print_descending(n - 1);
-}
-
 void print_stars(int n, int current = 1) {
     if (current > n) {
         return;
@@ -32,10 +16,6 @@ void print_stars(int n, int current = 1) {
 int main() {
     int N;
     cin >> N;
-    print_ascending(N);
-    cout << endl;
-    print_descending(N);
-    cout << endl;
     print_stars(N);
     return 0;
 }
